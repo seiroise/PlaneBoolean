32937d0195c9c4654835788808aff6fef21f99ae
seiroise <seiroise@gmail.com>
2016-11-07 13:26:09 +0900

    ビ追加

---
+++ Assets/Scripts/Camera/Aim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 追跡
+/// </summary>
+public class Aim : MonoBehaviour {
+
+	public Transform aimTarget;
+	public Vector3 positionOffset;
+
+	private bool[] positionConstrains;
+
+	#region UnityEvent
+
+
+
+	#endregion
+
+	#region Function
+
+	/// <summary>
+	/// 座標の追跡
+	/// </summary>
+	private void AimPosition() {
+		
+	}
+
+	#endregion
+}
